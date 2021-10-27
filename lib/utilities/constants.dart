ef0201be8d4d8d2f1401f1441e230579fddcd92b
sezinlemece <scoskunsu@yahoo.com>
2021-10-27 21:48:53 +0300

    Initial commit

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Righteous',
+  fontSize: 100.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Righteous',
+  fontSize: 50.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 30.0,
+  fontFamily: 'Righteous',
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 100.0,
+);
+
+const kTextFieldInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(
+    Icons.location_city,
+    color: Colors.white,
+  ),
+  hintText: 'Enter City Name',
+  hintStyle: TextStyle(
+    color: Colors.grey,
+  ),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10.0),
+    ),
+    borderSide: BorderSide.none,
+  ),
+);
